3f0e8eff9ecde81b4f1e8801c6e2ebdda277033a
Céline Cottier <cottier.celine@gmail.com>
2020-06-24 21:42:49 +0200

    improved burger menu and home page css

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -6,11 +6,6 @@ import { slide as Menu } from 'react-burger-menu'
 import './BurgerMenu.css'
  
 class BurgerMenu extends React.Component {
-  showSettings (event) {
-    event.preventDefault();
-    
-  }
- 
   render () {
     // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
     return (
@@ -20,7 +15,6 @@ class BurgerMenu extends React.Component {
         <a id="Diary" className="menu-item" href="/myDiary">My Diary</a>
         <a id="Treatment" className="menu-item" href="/myTreatment">My Treatment</a>
         <a id="history" className="menu-item" href="/myHistory">History</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
       </Menu>
     );
   }
