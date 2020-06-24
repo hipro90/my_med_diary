ff2545de785e41518cfee65f33ddcb4605180bda
Céline Cottier <cottier.celine@gmail.com>
2020-06-24 19:30:59 +0200

    improved burger menu and started home page

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -2,18 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import { slide as Menu } from 'react-burger-menu'
+
+import './BurgerMenu.css'
  
 class BurgerMenu extends React.Component {
   showSettings (event) {
     event.preventDefault();
-  
+    
   }
  
   render () {
     // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
     return (
       <Menu>
-        <a className="home" href="/">Home</a>
+        <a id="home" className="menu-item" href="/">Home</a>
         <a id="Pillbox" className="menu-item" href="/myPillbox">My Pillbox</a>
         <a id="Diary" className="menu-item" href="/myDiary">My Diary</a>
         <a id="Treatment" className="menu-item" href="/myTreatment">My Treatment</a>
