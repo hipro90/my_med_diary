c7ee49c7d6b170cc9a376dc43cd05a459db20a9c
hipro90 <hipro90@gmail.com>
2020-06-25 11:38:51 +0200

    move read in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom'
+import './Firebase/Config'
+import 'firebase/firestore'
+import * as firebase from 'firebase'
 
+import Delete from './Firebase/Delete'
 import BurgerMenu from './components/BurgerMenu'
 import Home from './components/Home'
 import Loader from './components/Loader'
@@ -9,13 +13,38 @@ import MyHistory from './components/MyHistory'
 import MyPillbox from './components/MyPillbox'
 import MyTreatment from './components/MyTreatment'
 
-
 import './App.css';
 
 const App = () => {
+  const db = firebase.firestore()
+  const [historique, setHistorique] = useState([])
+  const [medicament, setMedicament] = useState([])
+
+  useEffect(() => {
+      db.collection('historique').get()
+      .then(snapShot => {
+          const result = snapShot.docs.map(doc => doc.data())
+          setHistorique(result)
+      })
+      .catch(error => {
+          console.log("Error writing document:", error)
+      })
+
+      db.collection('medicament').get()
+      .then(snapShot => {
+          const result = snapShot.docs.map(doc => doc.data())
+          setMedicament(result)
+      })
+      .catch(error => {
+          console.log("Error writing document:", error)
+      })
+  },[])
+
   return (
     <div className="App">
-      <BurgerMenu/>
+      <h1>Test Firebase</h1>
+      
+      {/* <BurgerMenu/> */}
       <Switch>
         <Route exact path = '/' component = {Home} />
         <Route path = '/loader' component = {Loader} />
