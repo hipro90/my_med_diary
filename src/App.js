a63362080f10cabe54ea4bf1f0cee664f20a9b94
Moni <monica_be@hotmail.fr>
2020-06-25 10:41:54 +0200

    changing style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
-//import BurgerMenu from './components/BurgerMenu'
+import BurgerMenu from './components/BurgerMenu'
 import Home from './components/Home'
 import Loader from './components/Loader'
 import MyDiary from './components/MyDiary'
@@ -15,10 +15,10 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      {/* <BurgerMenu/> */}
+      <BurgerMenu/>
       <Switch>
-        <Route exact path = '/' component = {Home} />
-        <Route path = '/myLoader' component = {Loader} />
+        <Route exact path = '/' component = {Loader} />        
+        <Route path = '/Home' component = {Home} />
         <Route path = '/myDiary' component = {MyDiary} />
         <Route path = '/myHistory' component = {MyHistory} />
         <Route path = '/myPillbox' component = {MyPillbox} />
--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -10,7 +10,7 @@ class BurgerMenu extends React.Component {
     // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
     return (
       <Menu>
-        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="home" className="menu-item" href="/Home">Home</a>
         <a id="Pillbox" className="menu-item" href="/myPillbox">My Pillbox</a>
         <a id="Diary" className="menu-item" href="/myDiary">My Diary</a>
         <a id="Treatment" className="menu-item" href="/myTreatment">My Treatment</a>
