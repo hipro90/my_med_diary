9631786d3b17263574285a57b6eacdb9c2e79387
Céline Cottier <cottier.celine@gmail.com>
2020-06-25 11:01:22 +0200

    loader redirect to home after 2 seconds

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,22 +2,26 @@ import React, { useEffect, useState } from 'react'
 import MMDLogo from './../images/mmd-logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSmile } from '@fortawesome/free-solid-svg-icons'
+import { Redirect } from 'react-router-dom'
 
 import './Loader.css'
 
 const Loader = () =>{
     const [load, setLoad] = useState(false)
+
     useEffect(() => {
       setTimeout(() => {
         setLoad(true)
-      }, 3000)
+      }, 2000)
     }, [])
-    return(
+
+    return load ? <Redirect to='/Home' /> :(
         <div className='logo-container'>
             <img src={MMDLogo} alt='Logo My Med Daily' className='mmd-logo' />
             <FontAwesomeIcon icon={faSmile} className='icon' />
-        </div>
+         </div>
     )
 }
 
 export default Loader
+
