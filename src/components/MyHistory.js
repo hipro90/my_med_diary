136298a0f6c4abd99f35b2c90397a240c7c54a72
Moni <monica_be@hotmail.fr>
2020-06-25 23:52:20 +0200

    adding tilte

--- src/components/MyHistory.js
+++ src/components/MyHistory.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import PartsListing from './Test'
 import BurgerMenu from './BurgerMenu'
 
 const MyHistory = () => {
     return (
         <div>
             <BurgerMenu/>
+            <h1>My history</h1>
+            <PartsListing />
         </div>
     )
 }
