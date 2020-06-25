f6cb2d297df6c97b175f5faa2fef3dd97af0507f
MANAUT <tristanmanaut@gmail.com>
2020-06-25 22:02:41 +0200

    component formMedeciment ok

--- src/components/MyTreatment.js
+++ src/components/MyTreatment.js
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
-<<<<<<< HEAD
 import './MyTreatment.css'
 
 const MyTreatment = () => {
@@ -28,12 +27,6 @@ const MyTreatment = () => {
         <FontAwesomeIcon icon={faPlusCircle} className='iconMore' /></div>
         <div><Modal showModal={openModal} closeModal={closeModal} /></div>
         </>
-=======
-const MyTreatment = (props) => {
-    return (
-        <div>
-        </div>
->>>>>>> 9bc6daebc69856b9170bcdb53b3020400ceebd5e
     )
 }
 
